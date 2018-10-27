97d964721839d9d11dc64d40058fb34226887718
geenafong <geenafong@gmail.com>
2018-10-27 08:43:40 -0700

    update

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 // Variables to require the necessary dependencies.
 const express = require('express');
-// const data = require('data.json');
+const data = require('./data.json');
 const app = express();
 
 // //Middleware
-// app.set('view engine', 'pug');
-// app.use('/static', express.static('public'));
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+app.use('/images', express.static('images'));
 
 // //Routes
 // //An "index" route (/) to render the "Home" page with the locals set to data.projects
-// app.get('/', (req, res) => {
-//     res.render("Home");
-// });
+app.get('/', (req, res) => {
+    res.render('index', { data });
+});
 // //An "about" route (/about) to render the "About" page
-// app.get('/about', (req, res) => {
-//     res.render("About");
-// });
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 //Dynamic "project" routes
-
+app.get('/projects/:id', (req, res) => {
+    res.render('project', {
+        data:data,
+        id: req.params.id
+    })
+});
 
 //Listening on port 3000
 app.listen(3000, () => {
